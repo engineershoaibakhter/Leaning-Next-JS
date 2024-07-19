ba04ca8024364d9d6f8235bff06e3d11353018e8
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-07-19 11:52:09 +0500

    UI is completed

---
+++ learning-redux/src/app/components/AddUser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddUser = () => {
+  return (
+    <>
+    <h1>AddUser</h1>
+    <h3></h3>
+    </>
+  )
+}
+
+export default AddUser
---
+++ learning-redux/src/app/components/DisplayUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayUser = () => {
+  return (
+    <>
+    <h1>DisplayUser</h1>
+    <h3></h3>
+    </>
+  )
+}
+
+export default DisplayUser
+
