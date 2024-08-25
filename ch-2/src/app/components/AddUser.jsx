237d22a0cd2674155597c861b77f26dd2a7af9ac
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-08-25 16:00:42 +0500

    feat: add user

--- ch-2/src/app/components/AddUser.jsx
+++ ch-2/src/app/components/AddUser.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+import { addUser } from '../redux/slice';
+import { useDispatch } from 'react-redux';
 
 const AddUser = () => {
+  const dispatch=useDispatch();
+  const [name,setName]=useState("");
+
+  const userDispatch=()=>{
+    dispatch(addUser(name))
+  }
   return (
     <>
     <h1>AddUser</h1>
     <form>
-        <input type="text" name='name' />
-        <button>Submit</button>
+        <input type="text" name='name' onChange={(e)=>setName(e.target.value)} />
+        <button onClick={userDispatch()}>Submit</button>
     </form>
     </>
   )
