afa69dc27be91fc249332a126fca81c1e56d3c5f
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-08-27 18:55:11 +0500

    "Updated styles and added form elements to InputForm component"

--- redux-app/src/app/page.tsx
+++ redux-app/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     from-slate-100 via-slate-300 to-slate-300
     flex flex-col items-center px-2 md:p-4">
       <div className="w-full max-w-screen-md bg-black
-      p-4 md:p-20 lg:p-10 rounded-md flex flex-col gap-5">
+      p-4 md:p-20 lg:p-10 rounded-md flex flex-col gap-5 text-gray-300">
         <InputForm />
       </div>
     </div>
--- redux-app/src/components/InputForm.tsx
+++ redux-app/src/components/InputForm.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const InputForm = () => {
   return (
-    <div>InputForm</div>
+    <div>
+        {/* Todo Form */}
+        <form className='flex items-center gap-4 h-10 md:h-12'>
+            <input type="text" className='flex-1 h-full border-[1px] border-gray-600
+            bg-transparent pl-4 pr-6' />
+            <button>Add todo</button>
+        </form>
+        {/* TodoList */}
+    </div>
   )
 }
 
