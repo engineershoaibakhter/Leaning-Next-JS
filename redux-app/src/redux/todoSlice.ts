4aa3ad5534b0219c044a1484bea49f82179789c7
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-09-02 11:08:32 +0500

    "Added todoReducer to store configuration"

---
+++ redux-app/src/redux/todoSlice.ts
@@ -0,0 +1,31 @@
+import { Todo } from "@/type";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface TodoState{
+    todoList:Todo[]
+}
+
+const initialState:TodoState={
+todoList:[
+    // {_id:1,todo:"todo1"}
+],
+}
+
+export const todoSlice=createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action:PayloadAction<Todo>)=>{
+            state.todoList.push(action.payload);
+        },
+        deleteTodo:(state,action:PayloadAction<string>)=>{
+            state.todoList=state.todoList.filter((item)=>item._id !== action.payload)
+        },
+        removeTodo:(state)=>{
+            state.todoList=[];
+        }
+    }
+})
+
+export const {addTodo,deleteTodo,removeTodo} = todoSlice.actions;
+export default todoSlice.reducer;
