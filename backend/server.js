f112d248423540f8b86bcf7ffb36c786be121034
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-09-06 11:11:05 +0500

    "Added POST, PUT, and DELETE routes to server.js"

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,18 @@ app.get('/',(req,res)=>{
     res.send("Hello World");
 })
 
+app.post('/post',(req,res)=>{
+    res.send("Upload the article");
+})
+
+app.put("/put",(req,res)=>{
+    res.send("Update the article");
+})
+
+app.delete("/delete",(req,res)=>{
+    res.send("Delete the article");
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
